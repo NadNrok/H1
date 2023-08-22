0726032a3dc011d5e735298b9de0996e1e0a136d
NadNrok <karret4444@gmail.com>
2023-08-22 09:20:58 +0300

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        byte x = 4;
+        short y = 44;
+        int z = 444;
+        long a = 4444L;
+        float b = 44.4F;
+        double c = 444.4;
+        System.out.println(x + y);
+        System.out.println(z - a);
+        System.out.println(b * c);
+        System.out.println(c / x);
+
+
+    }
+}
